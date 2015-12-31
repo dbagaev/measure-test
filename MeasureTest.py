6ba86151ad4f2c44987fadaaf99a41bc83f4ff59
Dmytro Bagaiev <dmytro.bagaiev@gmail.com>
2015-12-31 17:38:59 +0100

    Can load list of tests

--- MeasureTest.py
+++ MeasureTest.py
@@ -6,11 +6,21 @@ class MeasuredTest :
     All = {}
 
     def __init__(self, obj) :
-        self.obj = obj
+        self.obj = obj        
         MeasuredTest.All[obj] = self
+        print("Registreing test: " + self.testType())
 
-    def __call__(self) :
-        print("Attribute called")
+    def findTests(self) :
+        print("Getting test list for " + self.testType())
+        try :
+            print(self.obj.findTests())
+            return self.obj.findTests()
+        except Exception as e:
+            print(e)
+            return []
+
+    def testType(self) :
+        return self.obj.__name__
 
 class OutputParameter :
     def __init__(self, obj) :
@@ -27,8 +37,14 @@ class OutputParameter :
 
 @MeasuredTest
 class DoTest :
-    def __init__(self) :
+    def __init__(self, name) :
         self.data = ""
+        self.name = name
+
+    @classmethod
+    def findTests(cls) :
+        print("in DoTest.findTests()")
+        return [cls('ghghgh')]
 
     def test(self) :
         print("DoTest.test()")
@@ -36,11 +52,16 @@ class DoTest :
 
 @MeasuredTest
 class DoOtherTest :
-    def __init__(self) :
+    def __init__(self, name) :
         self.data = ""
+        self.name = name
 
     def test(self) :
         print("DoOtherTest.test()")
+
+    @classmethod
+    def findTests(cls) :
+        return [cls('asasas')]
 
     @OutputParameter
     def Parameter1(self) :
